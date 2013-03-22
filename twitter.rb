2109c0ac60829efeff2fdd112a44fbbf5aafb43c
App Academy Student <student@appacademy.io>
2013-03-21 17:07:32 -0700

    Setup classes and most methods

---
+++ twitter.rb
@@ -0,0 +1,116 @@
+require 'launchy'
+require 'oauth'
+require 'yaml'
+require 'json'
+require 'pry'
+CONSUMER_KEY = "UpwSh5XirW3hlNljRcF3xw"
+CONSUMER_SECRET = "UgQhetUwdhDCus4nRphnPebMG4z1vmatEONmMFUEQ0"
+
+CONSUMER = OAuth::Consumer.new(
+  CONSUMER_KEY, CONSUMER_SECRET, :site => "https://twitter.com")
+
+class User
+
+  attr_accessor :statuses
+
+  def initialize
+    @statuses = []
+
+
+  end
+
+  def statuses
+    temp = []
+    user_timeline.each do |tweet|
+      temp <<  Status.new(EndUser::current_user, tweet["text"], tweet["created_at"])
+    end
+    @statuses = temp
+  end
+
+  def user_timeline
+    # the access token class has methods `get` and `post` to make
+    # requests in the same way as RestClient, except that these will be
+    # authorized. The token takes care of the crypto for us :-)
+    timeline = EndUser::access_token.get("http://api.twitter.com/1.1/statuses/user_timeline.json").body
+    JSON.parse(timeline)
+  end
+
+end
+
+class EndUser < User
+
+  attr_reader :name
+
+  def initialize(name)
+    super()
+    @name = name
+  end
+
+  def inspect
+    @name
+  end
+
+  def self.get_token(token_file)
+    # We can serialize token to a file, so that future requests don't need
+    # to be reauthorized.
+
+    if File.exist?(token_file)
+      File.open(token_file) { |f| YAML.load(f) }
+    else
+      access_token = self.request_access_token #ask
+      File.open(token_file, "w") { |f| YAML.dump(access_token, f) }
+
+      access_token
+    end
+  end
+
+  def self.access_token
+    @@access_token
+  end
+
+  def self.current_user
+    @@current_user
+  end
+
+  def self.login(username)
+    @@access_token = get_token(username)
+    @@current_user = EndUser.new(username)
+  end
+
+
+  def request_access_token
+    # send user to twitter URL to authorize application
+    request_token = CONSUMER.get_request_token
+    authorize_url = request_token.authorize_url
+    puts "Go to this URL: #{authorize_url}"
+    # launchy is a gem that opens a browser tab for us
+    Launchy.open(authorize_url)
+
+    # because we don't use a redirect URL; user will receive an "out of
+    # band" verification code that the application may exchange for a
+    # key; ask user to give it to us
+    puts "Login, and type your verification code in"
+    oauth_verifier = gets.chomp
+
+    # ask the oauth library to give us an access token, which will allow
+    # us to make requests on behalf of this user
+    access_token = request_token.get_access_token(
+        :oauth_verifier => oauth_verifier)
+  end
+
+end
+
+
+class Status
+
+  attr_reader :user, :message, :date
+
+  def initialize(user, message, date)
+    @user = user
+    @message = message
+    @date = date
+
+  end
+
+end
+
